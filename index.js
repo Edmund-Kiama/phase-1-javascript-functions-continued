acc0698fd42c16ba24c06fc53b1a6c04316e84b0
Edmund <edmundgachanja@gmail.com>
2024-11-26 14:19:10 +0300

    Created functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const saturdayFun = (arg = 'roller-skate') => `This Saturday, I want to ${arg}!`;
+const mondayWork = (arg = 'go to the office') => `This Monday, I will ${arg}.`;
+const wrapAdjective = (wrap) => {
+    return function (string) {
+        return `You are ${wrap}${string}${wrap}!`
+    }
+}
+console.log(wrapAdjective('!!!')("hello"))
